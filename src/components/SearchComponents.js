a8ecb0506e7a2df87a9346cf9759fa999f635d61
Laszlo-Lazuer <visualjazz@gmail.com>
2018-07-30 16:17:03 -0500

    Final version of search component.

--- src/components/SearchComponents.js
+++ src/components/SearchComponents.js
@@ -4,14 +4,29 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  TextInput,
+  TouchableHighlight
 } from 'react-native';
 
 export default class SearchComponent extends Component {
+
   render() {
     return (
       <View style={styles.main}>
         <Text style={styles.title}>Search For Github User</Text>
+        <TextInput
+              style={styles.searchInput}
+            />
+        <TouchableHighlight
+                style = {styles.button}
+                underlayColor= "white"
+              >
+              <Text
+                  style={styles.buttonText}>
+                  SEARCH
+              </Text>
+            </TouchableHighlight>
       </View>
     )
   }
@@ -30,5 +45,32 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     fontSize: 25,
     textAlign: 'center'
+  },
+  searchInput: {
+    height: 50,
+    padding: 4,
+    marginRight: 5,
+    fontSize: 23,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+    color: 'white'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor:'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
   }
 });
